fa995b83292b3f3d60386f8434024b0ac5dc2e20
gosha <gosha.titov@icloud.com>
2023-08-03 21:34:32 +0300

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# AtomicEngine
+
+A description of this package.
---
+++ Tests/AtomicEngineTests/AtomicEngineTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import AtomicEngine
+
+final class AtomicEngineTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(AtomicEngine().text, "Hello, World!")
+    }
+}

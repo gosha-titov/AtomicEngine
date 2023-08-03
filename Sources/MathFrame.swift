3b85db55faafe74af2dfd66241dba19438a31f0c
gosha <gosha.titov@icloud.com>
2023-08-03 23:29:13 +0300

    Feat: add the `findLis(of:)` method

--- Sources/MathFrame.swift
+++ Sources/MathFrame.swift
@@ -1,5 +1,67 @@
 internal final class MathFrame {
     
+    internal typealias Sequence = [Int]
+    internal typealias OptionalSequence = [Int?]
+    internal typealias Subsequence = [Int]
+    
+    
+    // MARK: Find Lis
+    
+    /// Finds the longest-increasing-subsequence of the given sequence.
+    ///
+    /// It's the main method on which all other operations are based.
+    ///
+    ///     let sequence = [2, 6, 0, 8, 1, 3, 1]
+    ///     let subsequence = findLis(of: sequence) // [0, 1, 3]
+    ///
+    /// The example sequence has two *lises*: `[2, 6, 8]` and `[0, 1, 3]`.
+    /// This method returns always the smallest one, that is `[0, 1, 3]`.
+    /// - Complexity: In the worst case, O(*n* log *n*), where *n* is the length of the sequence.
+    /// - Returns: The longest increasing subsequence of the sequence.
+    internal static func findLis(of sequence: Sequence) -> Subsequence {
+        
+        guard sequence.count > 1 else { return sequence }
+        
+        // The array contains the found lises of each length for the current step.
+        // Lises are ordered by the last element. The length of next lis is one longer.
+        // Therefore, the longest lis is the last one.
+        //
+        // Example: sequence = [0, 8, 4, 12, 2, 10, 6, 14, 1, 9, 5, 13, 3, 11, 7]
+        // At the last step, lises will be [[0], [0, 1], [0, 1, 3], [0, 1, 3, 7], [0, 2, 6, 9, 11]]
+        var lises: [Subsequence] = [[sequence.first!]]
+        
+        for element in sequence[1...] {
+            
+            var lowerBound = 0, upperBound = lises.count - 1
+            var index: Int { lowerBound }
+            
+            // Lises are ordered by the last element.
+            // Shift the boundaries to the first element that is bigger than the current one.
+            // Use binary search which is the fastest.
+            while lowerBound < upperBound {
+                let middle = lowerBound + (upperBound - lowerBound) / 2
+                let middleElement = lises[middle].last!
+                if middleElement == element { lowerBound = middle; break }
+                if middleElement > element  { upperBound = middle }
+                else { lowerBound = middle + 1 }
+            }
+            
+            // If all elements are smaller, then we add a new lis.
+            // If all elements are bigger, then we change the first lis.
+            // In any other case, we change the selected lis.
+            if index == lises.count - 1, element > lises[index].last! {
+                lises.append(lises[index] + [element])
+            } else if index == 0 {
+                lises[0] = [element]
+            } else {
+                lises[index] = lises[index - 1] + [element]
+            }
+        }
+        
+        return lises.last!
+    }
+    
+    
     // MARK: - Init
     
     /// Creates a math frame instance.

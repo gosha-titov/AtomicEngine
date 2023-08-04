749cbc7842d7ef4a5bee0ac9865dff0aa3d6c2b7
gosha <gosha.titov@icloud.com>
2023-08-04 05:11:25 +0300

    Chore: add extensions

--- Sources/Extensions/Array+.swift
+++ Sources/Extensions/Array+.swift
@@ -7,4 +7,32 @@ internal extension Array where Element: Numeric {
     ///
     var sum: Element { reduce(0, +) }
     
+    /// Returns the first K elements of this array.
+    ///
+    ///     let arr = [1, 2, 3, 4, 5]
+    ///     arr.first(3) // [1, 2, 3]
+    ///
+    func first(_ k: Int) -> [Element] {
+        let k = k > count ? count : k
+        var first = [Element]()
+        for i in 0..<k {
+            first.append(self[i])
+        }
+        return first
+    }
+    
+    /// Returns the last K elements of this array.
+    ///
+    ///     let arr = [1, 2, 3, 4, 5]
+    ///     arr.last(3) // [3, 4, 5]
+    ///
+    func last(_ k: Int) -> [Element] {
+        let k = k > count ? count : k
+        var last = [Element]()
+        for i in (count - k)..<count {
+            last.append(self[i])
+        }
+        return last
+    }
+    
 }
--- Sources/Extensions/String+.swift
+++ Sources/Extensions/String+.swift
@@ -1,5 +1,53 @@
 internal extension String {
     
+    /// Returns a string containing characters this string and the given string have in common,
+    /// starting from the ending of each up to the first characters that aren’t equivalent.
+    ///
+    ///     let str1 = "abcde"
+    ///     let str2 = "abde"
+    ///     str1.commonSuffix(with: str2) // "de"
+    ///
+    func commonSuffix(with str: String) -> String {
+        var suffix = String()
+        let min = min(count, str.count)
+        let str1 = self.last(min)
+        let str2 = str .last(min)
+        for (char1, char2) in zip(str1, str2).reversed() {
+            if char1 == char2 { suffix += char1 }
+            else { break }
+        }
+        return String(suffix.reversed())
+    }
+    
+    /// Returns the first K elements of the string.
+    ///
+    ///     let str = "abcde"
+    ///     str.first(3) // "abc"
+    ///
+    func first(_ k: Int) -> String {
+        let k = k > count ? count : k
+        var first = String()
+        for i in 0..<k {
+            first.append(self[i])
+        }
+        return first
+    }
+    
+    /// Returns the last K elements of the string.
+    ///
+    ///     let str = "abcde"
+    ///     str.last(3) // "cde"
+    ///
+    func last(_ k: Int) -> String {
+        let k = k > count ? count : k
+        var last = String()
+        for i in (count - k)..<count {
+            last.append(self[i])
+        }
+        return last
+    }
+    
+    
     // MARK: Operators
     
     static func += (lhs: inout String, rhs: Character) -> Void {

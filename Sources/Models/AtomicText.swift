0758eb0ae7dc736e94a139c011d5f4833cc56aae
gosha <gosha.titov@icloud.com>
2023-08-13 17:09:18 +0300

    Feat: add the `from(_:withType:)` static method

--- Sources/Models/AtomicText.swift
+++ Sources/Models/AtomicText.swift
@@ -1,1 +1,19 @@
 public typealias AtomicText = [AtomicCharacter]
+
+
+internal extension AtomicText {
+    
+    /// Creates an atomic text from a String value where all atomic characters are of the given type.
+    ///
+    ///     let atomicText = AtomicText.from("Hello", withType: .correct)
+    ///     /*[AtomicCharacter("H", type: .correct),
+    ///        AtomicCharacter("e", type: .correct),
+    ///        AtomicCharacter("l", type: .correct),
+    ///        AtomicCharacter("l", type: .correct),
+    ///        AtomicCharacter("o", type: .correct)]*/
+    ///
+    static func from(_ text: String, withType type: AtomicCharacter.AtomicType) -> AtomicText {
+        return text.map { AtomicCharacter(rawValue: $0, type: type) }
+    }
+    
+}

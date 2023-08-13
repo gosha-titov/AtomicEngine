c4b9341f9220c724989b8db25ba2784d05d7abe7
gosha <gosha.titov@icloud.com>
2023-08-13 17:26:26 +0300

    Feat: add the `leading(to:)` method

--- Sources/Models/AtomicText.swift
+++ Sources/Models/AtomicText.swift
@@ -13,7 +13,30 @@ internal extension AtomicText {
     ///        AtomicCharacter("o", type: .correct)]*/
     ///
     static func from(_ text: String, withType type: AtomicCharacter.AtomicType) -> AtomicText {
-        return text.map { AtomicCharacter(rawValue: $0, type: type) }
+        return text.map { AtomicCharacter($0, type: type) }
+    }
+    
+    /// Returns an atomic text leaded to the given letter case version.
+    ///
+    ///     let atomicText = AtomicText.from("hELlo", withType: .correct)
+    ///                                .leading(to: .lowercase)
+    ///     /*[AtomicCharacter("h", type: .correct),
+    ///        AtomicCharacter("e", type: .correct),
+    ///        AtomicCharacter("l", type: .correct),
+    ///        AtomicCharacter("l", type: .correct),
+    ///        AtomicCharacter("o", type: .correct)]*/
+    ///
+    func leading(to version: AtomicConfiguration.LetterCaseAction.Version) -> AtomicText {
+        var atomicText = self
+        var str = atomicText.map { $0.rawValue }.toString()
+        let types = atomicText.map { $0.type }
+        switch version {
+        case .capitalized: str.capitalize()
+        case .uppercase:   str.uppercase()
+        case .lowercase:   str.lowercase()
+        }
+        atomicText = zip(str, types).map { AtomicCharacter($0.0, type: $0.1) }
+        return atomicText
     }
     
 }

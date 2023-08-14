0bbabead00e70ec3e423018b1cd88395e622ca03
gosha <gosha.titov@icloud.com>
2023-08-14 12:59:27 +0300

    Feat: add the `checkQuickCompliance(for:relyingOn:to:)` static method.

--- Sources/BasicConverter.swift
+++ Sources/BasicConverter.swift
@@ -1,5 +1,34 @@
 internal final class BasicConverter {
     
+    // MARK: Pass Quick Compliance
     
+    /// Checks for quick compliance to the given configuration.
+    ///
+    /// This method finds max possible compliance, this means the compliance will be inaccurate for the better.
+    /// It allows you to find out in advance whether you need to do any complex calculations.
+    ///
+    /// For instance, the quick compliance is 70% when in fact the exact compliance is 50%.
+    /// But it cannot be that the quick compliance is 50% and the exact compliance is 70%.
+    ///
+    /// - Note: This method only checks for the presence or absence of chars, but not for their order.
+    /// - Returns: `True` if the compared text possibly satisfies all the conditions; otherwise, `false`.
+    @inlinable
+    internal static func checkQuickCompliance(for comparedText: String, relyingOn accurateText: String, to configuration: AtomicConfiguration) -> Bool {
+        
+        let countOfCommonChars = MathCore.countCommonChars(between: comparedText, and: accurateText)
+        
+        guard countOfCommonChars > 0 else { return false }
+        
+        if let requiredCount = configuration.requiredQuantityOfCorrectChars?.calculate(for: accurateText.count) {
+            guard requiredCount <= countOfCommonChars else { return false }
+        }
+        
+        if let acceptableCount = configuration.acceptableQuantityOfWrongChars?.calculate(for: comparedText.count) {
+            let countOfWrongChars = comparedText.count - countOfCommonChars
+            guard countOfWrongChars <= acceptableCount else { return false }
+        }
+        
+        return true
+    }
     
 }

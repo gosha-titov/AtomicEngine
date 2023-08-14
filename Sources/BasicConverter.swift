103f56a64fd08bc0dc0d4d94cb0ae842c94092b6
gosha <gosha.titov@icloud.com>
2023-08-14 13:41:30 +0300

    Feat: add the `checkExactCompliance(for:to:)` static method

--- Sources/BasicConverter.swift
+++ Sources/BasicConverter.swift
@@ -1,6 +1,35 @@
 internal final class BasicConverter {
     
-    // MARK: Pass Quick Compliance
+    // MARK: - Check Exact Compliance
+    
+    /// Checks for exact compliance to the given configuration.
+    ///
+    /// In contrast to the quick compliance, in order to check for the exact compliance this method needs a math basis.
+    /// This means the checking happens only after complex calculations, but the compliance is accurate.
+    ///
+    /// - Note: This method checks for the presence or absence of chars and for their order.
+    /// - Returns: `True` if the basis satisfies all the conditions; otherwise, `false`.
+    internal static func checkExactCompliance(for basis: MathCore.Basis, to configuration: AtomicConfiguration) -> Bool {
+        
+        guard !basis.subsequence.isEmpty else { return false }
+        
+        let accurateLength = basis.sourceSequence.count
+        if let requiredCount = configuration.requiredQuantityOfCorrectChars?.calculate(for: accurateLength) {
+            let countOfMatchingChars = basis.subsequence.count
+            guard requiredCount <= countOfMatchingChars else { return false }
+        }
+        
+        let comparedLength = basis.sequence.count
+        if let acceptableCount = configuration.acceptableQuantityOfWrongChars?.calculate(for: comparedLength) {
+            let countOfWrongChars = basis.sequence.count - basis.subsequence.count + basis.missingElements.count
+            guard countOfWrongChars <= acceptableCount else { return false }
+        }
+        
+        return true
+    }
+    
+    
+    // MARK: - Check Quick Compliance
     
     /// Checks for quick compliance to the given configuration.
     ///
@@ -9,6 +38,7 @@ internal final class BasicConverter {
     ///
     /// For instance, the quick compliance is 70% when in fact the exact compliance is 50%.
     /// But it cannot be that the quick compliance is 50% and the exact compliance is 70%.
+    /// That is, the accuaracy of the quick check is always higher or equal to the exact check.
     ///
     /// - Note: This method only checks for the presence or absence of chars, but not for their order.
     /// - Returns: `True` if the compared text possibly satisfies all the conditions; otherwise, `false`.
@@ -30,5 +60,11 @@ internal final class BasicConverter {
         
         return true
     }
+    
+    
+    // MARK: - Init
+    
+    /// Creates a basic converter instance.
+    private init() {}
     
 }

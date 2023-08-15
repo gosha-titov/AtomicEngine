0992955e78d9331bd6b2baaf0ab7223c05782b89
gosha <gosha.titov@icloud.com>
2023-08-15 11:49:42 +0300

    Feat: add the `init(from:type:)` initializer

--- Sources/Models/AtomicText.swift
+++ Sources/Models/AtomicText.swift
@@ -3,30 +3,20 @@ public typealias AtomicText = [AtomicCharacter]
 
 internal extension AtomicText {
     
-    /// Creates an atomic text from a String value where all atomic characters are of the given type.
-    ///
-    ///     let atomicText = AtomicText.from("Hello", withType: .correct)
-    ///     /*[AtomicCharacter("H", type: .correct),
-    ///        AtomicCharacter("e", type: .correct),
-    ///        AtomicCharacter("l", type: .correct),
-    ///        AtomicCharacter("l", type: .correct),
-    ///        AtomicCharacter("o", type: .correct)]*/
-    ///
-    static func from(_ text: String, withType type: AtomicCharacter.AtomicType) -> AtomicText {
-        return text.map { AtomicCharacter($0, type: type) }
-    }
-    
     /// Returns an atomic text leaded to the given letter case version.
     ///
-    ///     let atomicText = AtomicText.from("hELlo", withType: .correct)
-    ///                                .leading(to: .lowercase)
+    ///     let atomicText = AtomicText(
+    ///         from: "hELlo",
+    ///         withType: .correct
+    ///     ).leading(to: .lowercase)
+    ///
     ///     /*[AtomicCharacter("h", type: .correct),
     ///        AtomicCharacter("e", type: .correct),
     ///        AtomicCharacter("l", type: .correct),
     ///        AtomicCharacter("l", type: .correct),
     ///        AtomicCharacter("o", type: .correct)]*/
     ///
-    func leading(to version: AtomicConfiguration.LetterCaseAction.Version) -> AtomicText {
+    func leading(to version: AtomicConfiguration.LetterCaseVersion) -> AtomicText {
         var atomicText = self
         var str = atomicText.map { $0.rawValue }.toString()
         let types = atomicText.map { $0.type }
@@ -37,6 +27,36 @@ internal extension AtomicText {
         }
         atomicText = zip(str, types).map { AtomicCharacter($0.0, type: $0.1) }
         return atomicText
+    }
+    
+    /// Leads this atomic text to the given letter case version.
+    ///
+    ///     var atomicText = AtomicText(from: "hELlo", withType: .correct)
+    ///     atomicText.lead(to: .lowercase)
+    ///
+    ///     /*[AtomicCharacter("h", type: .correct),
+    ///        AtomicCharacter("e", type: .correct),
+    ///        AtomicCharacter("l", type: .correct),
+    ///        AtomicCharacter("l", type: .correct),
+    ///        AtomicCharacter("o", type: .correct)]*/
+    ///
+    mutating func lead(to version: AtomicConfiguration.LetterCaseVersion) -> Void {
+        self = leading(to: version)
+    }
+    
+    /// Creates an atomic text instance converted from the given string where all atomic characters are one-type.
+    ///
+    ///     let atomicText = AtomicText(from: "Hello", type: .correct)
+    ///
+    ///     /*[AtomicCharacter("H", type: .correct),
+    ///        AtomicCharacter("e", type: .correct),
+    ///        AtomicCharacter("l", type: .correct),
+    ///        AtomicCharacter("l", type: .correct),
+    ///        AtomicCharacter("o", type: .correct)]*/
+    ///
+    init(from text: String, type: AtomicCharacter.AtomicType) {
+        let atomicText = text.map { AtomicCharacter($0, type: type) }
+        self.init(atomicText)
     }
     
 }

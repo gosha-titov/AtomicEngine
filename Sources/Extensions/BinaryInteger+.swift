396482bafc8f1fb859cfcd56e6964c42c0ee29ca
gosha <gosha.titov@icloud.com>
2023-08-15 12:35:00 +0300

    Chore: add the `clamped(to:)` method

--- Sources/Extensions/BinaryInteger+.swift
+++ Sources/Extensions/BinaryInteger+.swift
@@ -7,4 +7,15 @@ internal extension BinaryInteger {
     ///
     var toDouble: Double { Double(self) }
     
+    /// Returns an integer clamped to the given limiting range.
+    ///
+    ///     let limits = 5...
+    ///     3.clamped(to: limits) // 5
+    ///     7.clamped(to: limits) // 7
+    ///
+    func clamped(to limits: PartialRangeFrom<Self>) -> Self {
+        if self < limits.lowerBound { return limits.lowerBound }
+        return self
+    }
+    
 }

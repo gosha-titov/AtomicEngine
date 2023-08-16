7dae0c25f2217358dc6239360bce58569d7beea9
gosha <gosha.titov@icloud.com>
2023-08-16 15:36:32 +0300

    Test: add tests for the `applying(_:to:)` method

---
+++ Tests/TextFormerTests.swift
@@ -0,0 +1,55 @@
+import XCTest
+@testable import AtomicEngine
+
+final class TextFormerTests: XCTestCase {
+
+    // MARK: - TextFormer.applying(_:to:)
+    
+    func testApplyingConfiguration() -> Void {
+        
+        var atomicText = AtomicText()
+        var configuration = AtomicConfiguration()
+        var result: AtomicText {
+            return TextFormer.applying(configuration, to: atomicText)
+        }
+        
+        atomicText = []
+        XCTAssertEqual(result, [])
+        
+        atomicText = [
+            AtomicCharacter("a", type: .correct),
+            AtomicCharacter("B", type: .missing),
+            AtomicCharacter("c", type: .extra  )
+        ]
+        
+        configuration.letterCaseAction = .compare
+        XCTAssertEqual(result, [
+            AtomicCharacter("a", type: .correct),
+            AtomicCharacter("B", type: .missing),
+            AtomicCharacter("c", type: .extra  )
+        ])
+        
+        configuration.letterCaseAction = .leadTo(.capitalized)
+        XCTAssertEqual(result, [
+            AtomicCharacter("A", type: .correct),
+            AtomicCharacter("b", type: .missing),
+            AtomicCharacter("c", type: .extra  )
+        ])
+        
+        configuration.letterCaseAction = .leadTo(.uppercase)
+        XCTAssertEqual(result, [
+            AtomicCharacter("A", type: .correct),
+            AtomicCharacter("B", type: .missing),
+            AtomicCharacter("C", type: .extra  )
+        ])
+        
+        configuration.letterCaseAction = .leadTo(.lowercase)
+        XCTAssertEqual(result, [
+            AtomicCharacter("a", type: .correct),
+            AtomicCharacter("b", type: .missing),
+            AtomicCharacter("c", type: .extra  )
+        ])
+        
+    }
+
+}

7499f382657dd8dcce18921d38882ded4befc99c
gosha <gosha.titov@icloud.com>
2023-08-19 19:25:04 +0300

    Feat: define the `TextEditor` class

---
+++ Sources/TextEditor.swift
@@ -0,0 +1,9 @@
+/// A text editor that consists of methods to make an atomic text user-friendly.
+internal final class TextEditor {
+    
+    // MARK: - Init
+    
+    /// Creates a text editor instance.
+    private init() {}
+    
+}

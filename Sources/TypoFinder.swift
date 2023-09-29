0631c418fda4d9e95dbc4625afa7f7850ae1067f
gosha <gosha.titov@icloud.com>
2023-09-29 16:14:34 +0300

    Feat: add `queue` and `configuration` properties

--- Sources/TypoFinder.swift
+++ Sources/TypoFinder.swift
@@ -2,4 +2,27 @@ import Foundation
 
 open class THTypoFinder {
     
+    // MARK: - Properties
+    
+    /// The queue that is used for **asynchronous** execution of all finding operations which go strickly after each other.
+    ///
+    /// This property has the default value that is created in the following way:
+    ///
+    ///     var queue = DispatchQueue(
+    ///         label: "com.typo-hunt.main",
+    ///         qos: .userInteractive
+    ///     )
+    ///
+    /// - Note: In the case when this property has no value (`nil`), the typo finding process is performed **asynchronously** on **the main thread**.
+    public var queue: DispatchQueue? = .init(label: "com.typo-hunt.main", qos: .userInteractive)
+    
+    /// The configuration consisting of parameters that are used during the creation of a text.
+    public var configuration = THConfiguration()
+    
+    
+    // MARK: - Init
+    
+    /// Creates a typo finder instance.
+    public init() {}
+    
 }

725cbcff9571b77da3355bd2ce37920653de2ee2
gosha <gosha.titov@icloud.com>
2023-10-03 14:26:32 +0300

    Feat: add the `updateDisplayedText(with:)` method

--- Sources/TextView.swift
+++ Sources/TextView.swift
@@ -1,13 +1,26 @@
 #if canImport(UIKit)
 import UIKit
 
+/// The view that can display a text containing typos, such as missing, misspell or extra characters.
+///
+///     finder.findTypos(
+///         in: "Hola", relyingOn: "Hello",
+///         andHandleResult: { text in
+///             textView.text = text
+///         }
+///     )
+///
 @available(iOS 13.0, *)
 open class THTextView: UIScrollView {
     
     // MARK: - Properties
     
     /// The text that is currently displayed, settable.
-    public var text = THText()
+    public var text = THText() {
+        didSet {
+           updateDisplayedText(with: text)
+        }
+    }
     
     
     // MARK: UI views
@@ -24,14 +37,14 @@ open class THTextView: UIScrollView {
     
     // MARK: UI attributes
     
-    /// The size for the monospaced system font that is used for displaying text, settable.
-    public var fontSize: CGFloat = 16.0
-    
     /// The color that is used to display completely correct text, settable.
     public var completelyCorrectColor: UIColor = .green
     
     /// The color that is used to display correct characters, settable.
     public var correctColor: UIColor = .black
+    
+    /// The color that is used to emphasize swapped characters, arrows below them and wrong letter cases, settable.
+    public var warningColor: UIColor = .yellow
     
     /// The color that is used to display extra characters and strikethrough lines, settable.
     public var wrongColor: UIColor = .red
@@ -39,14 +52,110 @@ open class THTextView: UIScrollView {
     /// The color that is used to display missing characters, settable.
     public var missingColor: UIColor = .lightGray
     
-    /// The color that is used to display swapped characters and arrows below them, settable.
-    public var swappedColor: UIColor = .yellow
+    /// The size for the monospaced font that is used for displaying text.
+    private let fontSize: CGFloat
+    
+    
+    // MARK: - Methods
+    
+    /// Updates a displayed text by replacing the current text with the given one.
+    ///
+    /// You usually don't call this method directly, instead you set a new text to the corresponding `text` property:
+    ///
+    ///     textView.text = newText
+    ///
+    public final func updateDisplayedText(with newText: THText) -> Void {
+        let upperMutableString = NSMutableAttributedString()
+        let centerMutableString = NSMutableAttributedString()
+        let lowerMutableString = NSMutableAttributedString()
+        let space = " ".toNSAttributedString.applying(font: .monospacedSystemFont(ofSize: fontSize, weight: .regular))
+        for char in newText {
+            let currentChar = char.rawValue.toNSAttributedString.applying(font: .monospacedSystemFont(ofSize: fontSize, weight: .regular))
+            switch char.type {
+            case .correct:
+                let correctChar = currentChar.applying(foregroundColor: correctColor)
+                upperMutableString.append(space)
+                centerMutableString.append(correctChar)
+                lowerMutableString.append(space)
+            case .swapped(let position):
+                let arrowSymbol: String
+                switch position {
+                case .left:  arrowSymbol = "←"
+                case .right: arrowSymbol = "→"
+                }
+                let swappedChar: NSAttributedString
+                if char.rawValue == " " {
+                    swappedChar = currentChar
+                        .applying(underline: .single, withColor: warningColor)
+                } else {
+                    swappedChar = currentChar
+                        .applying(foregroundColor: warningColor)
+                }
+                let arrow = arrowSymbol.toNSAttributedString.applying(foregroundColor: warningColor)
+                upperMutableString.append(space)
+                centerMutableString.append(swappedChar)
+                lowerMutableString.append(arrow)
+            case .missing:
+                let missingChar: NSAttributedString
+                if char.rawValue == " " {
+                    missingChar = currentChar
+                        .applying(underline: .single, withColor: missingColor)
+                } else {
+                    missingChar = currentChar
+                        .applying(foregroundColor: missingColor)
+                }
+                upperMutableString.append(space)
+                centerMutableString.append(missingChar)
+                lowerMutableString.append(space)
+            case .extra:
+                let extraChar: NSAttributedString
+                if char.rawValue == " " {
+                    extraChar = currentChar
+                        .applying(underline: .single, withColor: wrongColor)
+                } else {
+                    extraChar = currentChar
+                        .applying(foregroundColor: correctColor)
+                        .applying(strikethrough: 1, withColor: wrongColor)
+                }
+                upperMutableString.append(space)
+                centerMutableString.append(extraChar)
+                lowerMutableString.append(space)
+            case .misspell(let correctSymbol):
+                let misspellChar: NSAttributedString
+                let correctChar: NSAttributedString
+                if char.rawValue == " " {
+                    misspellChar = currentChar
+                        .applying(underline: .single, withColor: wrongColor)
+                } else {
+                    misspellChar = currentChar
+                        .applying(foregroundColor: correctColor)
+                        .applying(strikethrough: 1, withColor: wrongColor)
+                }
+                if correctSymbol == " " {
+                    correctChar = currentChar
+                        .applying(underline: .single, withColor: wrongColor)
+                } else {
+                    correctChar = correctSymbol.toNSAttributedString
+                        .applying(foregroundColor: wrongColor)
+                }
+                upperMutableString.append(correctChar)
+                centerMutableString.append(misspellChar)
+                lowerMutableString.append(space)
+            }
+        }
+        upperLabel.attributedText = upperMutableString
+        textLabel.attributedText = centerMutableString
+        lowerLabel.attributedText = lowerMutableString
+    }
     
     
     // MARK: - Init
     
     /// Creates a text view with the specified frame rectangle.
-    public override init(frame: CGRect) {
+    /// - Parameter frame: The frame rectangle for the view, measured in points.
+    /// - Parameter fontSize: The size (in points) for the monospaced font that is used for displaying text.
+    public init(frame: CGRect, fontSize: CGFloat = 16.0) {
+        self.fontSize = fontSize
         super.init(frame: frame)
         addSubviews()
         setupLabels()
@@ -58,7 +167,7 @@ open class THTextView: UIScrollView {
     }
     
     
-    // MARK: - Methods
+    // MARK: - Setup Methods
     
     private func setupLabels() -> Void {
         for label in [upperLabel, textLabel, lowerLabel] {
@@ -74,7 +183,7 @@ open class THTextView: UIScrollView {
     }
     
     
-    // MARK: Setup Layout
+    // MARK: Layout
     
     private func setupLayout() -> Void {
         applyConstraintsToUpperLabel()
@@ -85,7 +194,7 @@ open class THTextView: UIScrollView {
     private func applyConstraintsToUpperLabel() -> Void {
         upperLabel.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
-            upperLabel.topAnchor.constraint(equalTo: topAnchor),
+            upperLabel.bottomAnchor.constraint(equalTo: textLabel.topAnchor),
             upperLabel.leadingAnchor.constraint(equalTo: leadingAnchor),
             upperLabel.trailingAnchor.constraint(equalTo: trailingAnchor),
         ])
@@ -94,9 +203,9 @@ open class THTextView: UIScrollView {
     private func applyConstraintsToTextLabel() -> Void {
         textLabel.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
-            textLabel.topAnchor.constraint(equalTo: upperLabel.bottomAnchor),
             textLabel.leadingAnchor.constraint(equalTo: leadingAnchor),
             textLabel.trailingAnchor.constraint(equalTo: trailingAnchor),
+            textLabel.centerYAnchor.constraint(equalTo: centerYAnchor),
         ])
     }
     
@@ -106,18 +215,7 @@ open class THTextView: UIScrollView {
             lowerLabel.topAnchor.constraint(equalTo: textLabel.bottomAnchor),
             lowerLabel.leadingAnchor.constraint(equalTo: leadingAnchor),
             lowerLabel.trailingAnchor.constraint(equalTo: trailingAnchor),
-            lowerLabel.bottomAnchor.constraint(equalTo: bottomAnchor),
         ])
-    }
-    
-    
-    // MARK: Other
-    
-    private func createEmptyLabel() -> UILabel {
-        let label = UILabel()
-        label.backgroundColor = .clear
-        label.font = .monospacedSystemFont(ofSize: fontSize, weight: .regular)
-        return label
     }
     
 }
